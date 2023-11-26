d4fbc0a2f275c524a79a56a70863482d2d7328eb
infinifen <fen@mail.thefen.me>
2023-11-26 11:46:18 +0100

    add district gps area to db

--- GdanskExplorer/Data/District.cs
+++ GdanskExplorer/Data/District.cs
@@ -6,6 +6,7 @@ public class District
 {
     public Guid Id { get; set; }
     public Polygon Geometry { get; set; } = null!;
+    public Polygon GpsGeometry { get; set; } = null!;
     public double Area { get; set; }
     public string Name { get; set; } = null!;
     public List<DistrictAreaCacheEntry> DistrictAreaCacheEntries { get; set; } = new();
