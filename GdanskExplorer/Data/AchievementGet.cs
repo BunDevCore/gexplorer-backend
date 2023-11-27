608b5a8f071d16182606b535d66b49272d314e95
infinifen <fen@mail.thefen.me>
2023-11-27 14:36:08 +0100

    change achievement pk to string

--- GdanskExplorer/Data/AchievementGet.cs
+++ GdanskExplorer/Data/AchievementGet.cs
@@ -2,7 +2,7 @@ namespace GdanskExplorer.Data;
 
 public class AchievementGet
 {
-    public Guid AchievementId { get; set; }
+    public string AchievementId { get; set; }
     public Guid UserId { get; set; }
     public User User { get; set; } = null!;
     public DateTime TimeAchieved { get; set; }
