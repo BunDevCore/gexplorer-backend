a96102880cc51071a5d3abe32d41db23f33e665a
infinifen <fen@mail.thefen.me>
2023-11-28 06:39:54 +0100

    reduce code duplication in user controller

--- GdanskExplorer/Controllers/UserController.cs
+++ GdanskExplorer/Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using AutoMapper;
 using GdanskExplorer.Data;
 using GdanskExplorer.Dtos;
@@ -18,50 +19,37 @@ public class UserController : ControllerBase
         _mapper = mapper;
     }
 
-    [HttpGet("{username}")]
-    public async Task<ActionResult<UserReturnDto>> GetByUsername(string username)
+    public async Task<ActionResult<UserReturnDto>> HandleSearch(Expression<Func<User, bool>> condition)
     {
         var user = await _db.Users
-            .Include(x =>
-                x.Trips.OrderByDescending(t => t.UploadTime))
-            .Include(x => x.DistrictAreas)
-            .Include(x => x.Achievements)
-            .SimplifyUser()
-            .Where(x => x.UserName == username)
-            .FirstOrDefaultAsync();
+                    .Include(x =>
+                        x.Trips.OrderByDescending(t => t.UploadTime))
+                    .Include(x => x.DistrictAreas)
+                    .Include(x => x.Achievements)
+                    .SimplifyUser()
+                    .Where(condition)
+                    .FirstOrDefaultAsync();
+                
         
+                if (user is null)
+                {
+                    return NotFound();
+                }
+                
+                foreach (var trip in user.Trips)
+                {
+                    trip.User = user;
+                    _db.Entry(trip).State = EntityState.Unchanged; // make ef think nothing has changed because, well, it hasn't and i just know better, this all arises from SimplifyUser because i do not want the whole polygon loaded here
+                }
+                
+                return Ok(_mapper.Map<UserReturnDto>(user));
+    }
 
-        if (user is null)
-        {
-            return NotFound();
-        }
-        
-        foreach (var trip in user.Trips)
-        {
-            trip.User = user;
-            _db.Entry(trip).State = EntityState.Unchanged; // make ef think nothing has changed because, well, it hasn't and i just know better, this all arises from SimplifyUser because i do not want the whole polygon loaded here
-        }
-        
-        return Ok(_mapper.Map<UserReturnDto>(user));
-    }
-    
+    [HttpGet("{username}")]
+    public async Task<ActionResult<UserReturnDto>> GetByUsername(string username) =>
+        await HandleSearch(x => x.UserName == username);
+
     [HttpGet("id/{id:guid}")]
-    public async Task<ActionResult<UserReturnDto>> GetById(Guid id)
-    {
-        var user = await _db.Users
-            .Include(x =>
-                x.Trips.OrderByDescending(t => t.UploadTime))
-            .Include(x => x.DistrictAreas)
-            .Include(x => x.Achievements)
-            .SimplifyUser()
-            .Where(x => x.Id == id)
-            .FirstOrDefaultAsync();
-        
-        if (user is null)
-        {
-            return NotFound();
-        }
-        
-        return Ok(_mapper.Map<UserReturnDto>(user));
-    }
+    public async Task<ActionResult<UserReturnDto>> GetById(Guid id) =>
+        await HandleSearch(x => x.Id == id);
 }
