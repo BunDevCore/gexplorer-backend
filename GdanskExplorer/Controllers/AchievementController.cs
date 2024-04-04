479d241761c7c6f521e2a449d388fc8647e28ee3
infinifen <fen@mail.thefen.me>
2024-04-04 04:45:47 +0200

    handle secret achievements in achievement controller

--- GdanskExplorer/Controllers/AchievementController.cs
+++ GdanskExplorer/Controllers/AchievementController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using GdanskExplorer.Data;
 using GdanskExplorer.Dtos;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GdanskExplorer.Controllers;
@@ -10,18 +11,21 @@ public class AchievementController : ControllerBase
 {
     private readonly GExplorerContext _db;
     private readonly IMapper _mapper;
+    private readonly UserManager<User> _userManager;
 
 
-    public AchievementController(GExplorerContext db, IMapper mapper)
+
+    public AchievementController(GExplorerContext db, IMapper mapper, UserManager<User> userManager)
     {
         _db = db;
         _mapper = mapper;
+        _userManager = userManager;
     }
 
     [HttpGet("")]
     public IEnumerable<string> GetAll()
     {
-        return _db.Achievements.Select(x => x.Id);
+        return _db.Achievements.Where(x => !x.IsSecret).Select(x => x.Id);
     }
 
     [HttpGet("{achievementId}")]
@@ -30,6 +34,25 @@ public class AchievementController : ControllerBase
         var achievement = await _db.Achievements.FindAsync(achievementId);
 
         if (achievement is null)
+        {
+            return NotFound();
+        }
+
+        // regular achievement, just go return it like a sane person
+        if (!achievement.IsSecret) return Ok(_mapper.Map<AchievementDetailsDto>(achievement));
+        
+        // shh! don't show secret achievements to someone who hasn't already gotten them...
+        try
+        {
+            var userId = Guid.Parse(_userManager.GetUserId(User) ?? string.Empty);
+            if (achievement.AchievementGets.Any(x => x.UserId == userId))
+            {
+                return Ok(_mapper.Map<AchievementDetailsDto>(achievement));
+            }
+
+            return NotFound();
+        }
+        catch
         {
             return NotFound();
         }
