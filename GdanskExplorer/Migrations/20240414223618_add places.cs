e785266e9dc81a98b37c578890da879b44a4323e
infinifen <fen@mail.thefen.me>
2024-04-15 00:39:12 +0200

    add places migration

---
+++ GdanskExplorer/Migrations/20240414223618_add places.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GdanskExplorer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addplaces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Places",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Places", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlaceVisitedRows",
+                columns: table => new
+                {
+                    PlaceId = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Saved = table.Column<bool>(type: "boolean", nullable: false),
+                    Visited = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlaceVisitedRows", x => new { x.PlaceId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_PlaceVisitedRows_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlaceVisitedRows_Places_PlaceId",
+                        column: x => x.PlaceId,
+                        principalTable: "Places",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlaceVisitedRows_UserId",
+                table: "PlaceVisitedRows",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DatabaseLeaderboardRow");
+
+            migrationBuilder.DropTable(
+                name: "PlaceVisitedRows");
+
+            migrationBuilder.DropTable(
+                name: "Places");
+        }
+    }
+}
