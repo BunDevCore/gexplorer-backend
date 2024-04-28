80bc0dc08657b97be1fb63bbb2607cbd41fcd5ec
infinifen <48841404+infinifen@users.noreply.github.com>
2024-04-28 17:09:13 +0200

    still unused new functionality for tripmanager - trip import split out into multiple methods, overall area recompute

--- GdanskExplorer/TripManager.cs
+++ GdanskExplorer/TripManager.cs
@@ -53,35 +53,10 @@ public class TripManager
 
             // convert to database entities
             var dbTrips = tripTopologies.Select(topology =>
-                new Trip
-                {
-                    Id = new Guid(),
-                    User = user,
-                    GpsLineString = topology.GpsPath,
-                    GpsPolygon = topology.GpsPolygon,
-                    Polygon = topology.AreaPolygon,
-                    UploadTime = uploadTime,
-                    Area = topology.AreaPolygon.Area,
-                    Length = topology.LocalLinestring.Length,
-                    NewArea = topology.AreaPolygon.Difference(oldOverallArea).Area,
-                    StartTime = topology.StartTime,
-                    EndTime = topology.EndTime
-                }
+                TopologyToTripEntity(user, topology, uploadTime, oldOverallArea)
             ).ToList();
 
-            // update district caches
-            // ReSharper disable once EntityFramework.NPlusOne.IncompleteDataQuery
-            var newAreaCacheEntries = _db.Districts.AsParallel().Select(district =>
-                new DistrictAreaCacheEntry
-                {
-                    District = district,
-                    DistrictId = district.Id,
-                    User = user,
-                    UserId = user.Id,
-                    // ReSharper disable once EntityFramework.NPlusOne.IncompleteDataUsage
-                    Area = district.Geometry.Intersection(user.OverallArea).Area,
-                }
-            );
+            var newAreaCacheEntries = UpdateDistrictAreas(user);
 
             // purge all the previous ones for this user /shrug
             if (await _db.DistrictAreaCacheEntries.AnyAsync(x => x.UserId == user.Id))
@@ -102,6 +77,81 @@ public class TripManager
 
             await _db.SaveChangesAsync();
             return dbTrips;
+    }
+
+    public Trip TopologyToTripEntity(User user, TripTopologyInfo topology, DateTime uploadTime, MultiPolygon oldOverallArea)
+    {
+        return new Trip
+        {
+            Id = new Guid(),
+            User = user,
+            GpsLineString = topology.GpsPath,
+            GpsPolygon = topology.GpsPolygon,
+            Polygon = topology.AreaPolygon,
+            UploadTime = uploadTime,
+            Area = topology.AreaPolygon.Area,
+            Length = topology.LocalLinestring.Length,
+            NewArea = topology.AreaPolygon.Difference(oldOverallArea).Area,
+            StartTime = topology.StartTime,
+            EndTime = topology.EndTime
+        };
+    }
+
+    public ParallelQuery<DistrictAreaCacheEntry> UpdateDistrictAreas(User user)
+    {
+        // update district caches
+        // ReSharper disable once EntityFramework.NPlusOne.IncompleteDataQuery
+        var newAreaCacheEntries = _db.Districts.AsParallel().Select(district =>
+            new DistrictAreaCacheEntry
+            {
+                District = district,
+                DistrictId = district.Id,
+                User = user,
+                UserId = user.Id,
+                // ReSharper disable once EntityFramework.NPlusOne.IncompleteDataUsage
+                Area = district.Geometry.Intersection(user.OverallArea).Area,
+            }
+        );
+        return newAreaCacheEntries;
+    }
+
+    public async Task RecomputeOverallArea(User user, List<Trip> trips)
+    {
+        // initial sanity check
+        if (trips.Any(x => x.UserId != user.Id))
+        {
+            throw new ArgumentException("user does not own all trips!", nameof(trips));
+        }
+
+        if (trips.Count == 0)
+        {
+            user.OverallArea = MultiPolygon.Empty;
+            await _db.DistrictAreaCacheEntries.Where(x => x.UserId == user.Id)
+                .ExecuteUpdateAsync(
+                    x => x.SetProperty(dace => dace.Area, 0));
+            await _db.SaveChangesAsync();
+            return;
+        }
+        
+        trips.Sort((x, y) => x.UploadTime.CompareTo(y.UploadTime));
+        var tx = await _db.Database.BeginTransactionAsync();
+        var newOverallArea = await Task.Run(() =>
+        {
+            var overallArea = MultiPolygon.Empty as Geometry;
+            
+            foreach (var trip in trips)
+            {
+                trip.NewArea = trip.Polygon.Difference(overallArea).Area;
+                overallArea = overallArea.Union(trip.Polygon);
+            }
+
+            return overallArea;
+        }) as MultiPolygon;
+
+        user.OverallArea = newOverallArea!; // i don't think there's a way for this to turn out to be null with all the checks above and whatnot
+        user.OverallAreaAmount = newOverallArea!.Area;
+        await _db.SaveChangesAsync();
+        await tx.CommitAsync();
     }
 
     private async Task<IEnumerable<AchievementGet>> GetNewAchievements(User user, List<Trip> dbTrips)
